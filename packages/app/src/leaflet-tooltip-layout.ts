ecf9dacfce69f6266ad23026eddd67dfcffca5cf
Elliot <elliot@bitoffdev.com>
2022-05-14 20:17:15 -0400

    Light refactoring

--- packages/app/src/leaflet-tooltip-layout.ts
+++ packages/app/src/leaflet-tooltip-layout.ts
@@ -2,21 +2,22 @@ import { LineString, MultiLineString } from "geojson";
 import L from "leaflet";
 
 // global variables
-var map: L.Map;
-var markerList: Array<SpecialMarker> = []; // all markers here
-var polylineList: Array<L.Polyline> = []; // all polylines here
+let map: L.Map;
+const markerList: Array<SpecialMarker> = []; // all markers here
+const polylineList: Array<L.Polyline> = []; // all polylines here
 
 type PolylineCreatedCallback =
   | ((ply: L.Polyline<LineString | MultiLineString, any>) => void)
   | null;
 
 // events
-var _onPolylineCreated: PolylineCreatedCallback = null; // will be called after polyline has been created
+let _onPolylineCreated: PolylineCreatedCallback = null; // will be called after polyline has been created
 
 interface SpecialMarker extends L.Marker {
   __ply: L.Polyline<LineString | MultiLineString, any>;
   disp: L.Point;
-  _icon: any;
+  // _icon is a private member exposed by Leaflet that we need to access
+  _icon: HTMLElement;
 }
 
 interface SpecialTooltip extends L.Tooltip {
@@ -29,7 +30,7 @@ export function initialize(
   onPolylineCreated?: PolylineCreatedCallback
 ) {
   map = leafletMap;
-  polylineList = [];
+  polylineList.length = 0;
 
   //default style
   if (onPolylineCreated) {
@@ -103,11 +104,10 @@ export function getMarkers() {
 }
 
 function removeAllPolyline(map: L.Map) {
-  var i;
-  for (i = 0; i < polylineList.length; i++) {
+  for (let i = 0; i < polylineList.length; i++) {
     map.removeLayer(polylineList[i]);
   }
-  polylineList = [];
+  polylineList.length = 0;
 }
 
 /**
@@ -116,32 +116,24 @@ function removeAllPolyline(map: L.Map) {
  */
 function drawLine(map: L.Map) {
   removeAllPolyline(map);
-  for (var i = 0; i < markerList.length; i++) {
+  for (let i = 0; i < markerList.length; i++) {
     const marker: SpecialMarker = markerList[i] as SpecialMarker;
-    var markerDom = marker._icon;
-    var markerPosition = getPosition(markerDom);
-    var label = marker.getTooltip() as SpecialTooltip;
+    const { x: markerX, y: markerY } = getPosition(marker._icon);
+    const label = marker.getTooltip() as SpecialTooltip;
+    const labelDom = label._container;
+    let { x: labelX, y: labelY } = getPosition(labelDom);
 
-    var labelDom = label._container;
-    var labelPosition = getPosition(labelDom);
-
-    var x1 = labelPosition.x;
-    var y1 = labelPosition.y;
-
-    var x = markerPosition.x;
-    var y = markerPosition.y;
-
-    x1 -= 5;
-    y1 += 2;
-    if (x1 - x !== 0 || y1 - y !== 0) {
-      if (x1 + labelDom.offsetWidth < markerPosition.x) {
-        x1 += labelDom.offsetWidth;
+    labelX -= 5;
+    labelY += 2;
+    if (labelX - markerX !== 0 || labelY - markerY !== 0) {
+      if (labelX + labelDom.offsetWidth < markerX) {
+        labelX += labelDom.offsetWidth;
       }
-      if (y1 + labelDom.offsetHeight < markerPosition.y) {
-        y1 += labelDom.offsetHeight;
+      if (labelY + labelDom.offsetHeight < markerY) {
+        labelY += labelDom.offsetHeight;
       }
-      var lineDest = L.point(x1, y1);
-      var destLatLng = map.layerPointToLatLng(lineDest);
+      const lineDest = L.point(labelX, labelY);
+      const destLatLng = map.layerPointToLatLng(lineDest);
 
       setTimeout(
         ((marker, destLatLng) => () => {
@@ -158,17 +150,14 @@ function drawLine(map: L.Map) {
 }
 
 function setRandomPos() {
-  for (var i = 0; i < markerList.length; i++) {
-    var marker = markerList[i];
-    var label = marker.getTooltip() as SpecialTooltip;
-    var labelDom = label._container;
-    var markerDom = marker._icon;
-    var markerPosition = getPosition(markerDom);
+  for (let i = 0; i < markerList.length; i++) {
+    const marker = markerList[i];
+    const label = marker.getTooltip() as SpecialTooltip;
+    const labelDom = label._container;
+    const { x, y } = getPosition(marker._icon);
     // var angle = Math.floor(Math.random() * 19 + 1) * 2 * Math.PI / 20;
-    var angle = ((2 * Math.PI) / 6) * i;
-    var x = markerPosition.x;
-    var y = markerPosition.y;
-    var dest = L.point(
+    const angle = ((2 * Math.PI) / 6) * i;
+    const dest = L.point(
       Math.ceil(x + 50 * Math.sin(angle)),
       Math.ceil(y + 50 * Math.cos(angle))
     );
@@ -180,25 +169,21 @@ const scaleTo = (a: L.Point, b: L.Point): L.Point =>
   L.point(a.x * b.x, a.y * b.y);
 
 function normalize(a: L.Point) {
-  var l = a.distanceTo(L.point(0, 0));
+  const l = a.distanceTo(L.point(0, 0));
   if (l === 0) {
     return a;
   }
   return L.point(a.x / l, a.y / l);
 }
 
-function fa(x: number, k: number) {
-  return (x * x) / k;
-}
+const fa = (x: number, k: number): number => (x * x) / k;
 
-function fr(x: number, k: number) {
-  return (k * k) / x;
-}
+const fr = (x: number, k: number): number => (k * k) / x;
 
 /**
  * get position form el.style.transform
  */
-function getPosition(el: HTMLElement) {
+function getPosition(el: HTMLElement): L.Point {
   const translateString = el.style.transform
     .split("(")[1]
     .split(")")[0]
@@ -210,12 +195,12 @@ function getPosition(el: HTMLElement) {
  * t is the temperature in the system
  */
 function computePositionStep(t: number) {
-  var area = (window.innerWidth * window.innerHeight) / 10;
-  var k = Math.sqrt(area / markerList.length);
-  var dpos = L.point(0, 0);
-  var v_pos;
-  var v;
-  var i;
+  const area = (window.innerWidth * window.innerHeight) / 10;
+  const k = Math.sqrt(area / markerList.length);
+  let dpos = L.point(0, 0);
+  let v_pos: L.Point;
+  let v: SpecialMarker;
+  let i: number;
 
   for (i = 0; i < markerList.length; i++) {
     v = markerList[i];
@@ -224,10 +209,12 @@ function computePositionStep(t: number) {
     v_pos = getPosition((v.getTooltip()! as SpecialTooltip)._container);
 
     // compute gravitational force
-    for (var j = 0; j < markerList.length; j++) {
-      var u = markerList[j];
+    for (let j = 0; j < markerList.length; j++) {
+      const u = markerList[j];
       if (i !== j) {
-        var u_pos = getPosition((u.getTooltip()! as SpecialTooltip)._container);
+        const u_pos = getPosition(
+          (u.getTooltip()! as SpecialTooltip)._container
+        );
         dpos = v_pos.subtract(u_pos);
         // if (true dpos !== 0) {
         v.disp = v.disp.add(
@@ -253,9 +240,9 @@ function computePositionStep(t: number) {
     const leafletTooltip = markerList[i].getTooltip() as SpecialTooltip;
     if (leafletTooltip === undefined) continue;
 
-    var disp = markerList[i].disp;
-    var p = getPosition(leafletTooltip._container);
-    var d = scaleTo(
+    const disp = markerList[i].disp;
+    let p = getPosition(leafletTooltip._container);
+    const d = scaleTo(
       normalize(disp),
       L.point(Math.min(Math.abs(disp.x), t), Math.min(Math.abs(disp.y), t))
     );
@@ -266,19 +253,19 @@ function computePositionStep(t: number) {
 }
 
 function layoutByForce() {
-  var start = Math.ceil(window.innerWidth / 10);
-  var times = 10; // 50;
-  var t;
-  for (var i = 0; i < times; i += 1) {
-    t = start * (1 - i / (times - 1));
-    computePositionStep(t);
+  const start = Math.ceil(window.innerWidth / 10);
+  const times = 50;
+  let temperature: number;
+  let i;
+  for (i = 0; i < times; i += 1) {
+    temperature = start * (1 - i / (times - 1));
+    computePositionStep(temperature);
   }
 
   for (i = 0; i < markerList.length; i++) {
     const leafletTooltip = markerList[i].getTooltip() as SpecialTooltip;
     if (leafletTooltip === undefined) continue;
 
-    // var disp = markerList[i].disp;
     let p = getPosition(leafletTooltip._container);
     const { offsetWidth: width, offsetHeight: height } =
       leafletTooltip._container;
@@ -288,20 +275,20 @@ function layoutByForce() {
 }
 
 function setEdgePosition() {
-  var bounds = map.getBounds();
-  var northWest = map.latLngToLayerPoint(bounds.getNorthWest());
-  var southEast = map.latLngToLayerPoint(bounds.getSouthEast());
+  const bounds = map.getBounds();
+  const northWest = map.latLngToLayerPoint(bounds.getNorthWest());
+  const southEast = map.latLngToLayerPoint(bounds.getSouthEast());
 
   for (let i = 0; i < markerList.length; i++) {
     const leafletTooltip = markerList[i].getTooltip() as SpecialTooltip;
     if (leafletTooltip === undefined) continue;
 
-    var tooltip = getPosition(leafletTooltip._container);
-    var marker = getPosition(markerList[i]._icon);
+    const tooltip = getPosition(leafletTooltip._container);
+    const marker = getPosition(markerList[i]._icon);
     const { offsetWidth: width, offsetHeight: height } =
       leafletTooltip._container;
 
-    var isEdge = false;
+    let isEdge = false;
     if (marker.x > northWest.x && tooltip.x < northWest.x) {
       tooltip.x = northWest.x;
       isEdge = true;
